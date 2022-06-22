f4617d71d6197f1d0406db7bb1e16c80e621ebab
hafsadev290 <83162995+hafsadev290@users.noreply.github.com>
2022-06-22 17:01:00 +0100

    Update MyResume.tsx

--- components/MyResume.tsx
+++ components/MyResume.tsx
@@ -5,7 +5,7 @@ const MyResume = () => {
         <div className="mt-16 mb-8 text-center">
             <h1 className="justify-center text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
                 Wanna check my full
-                <a href="https://drive.google.com/file/d/1Cpam4MDRoQT9WJcKnXdiINUBXKmp6bOE/view?usp=sharing" target={"_blank"} className={"ml-6 font-medium tracking-wider transition-colors text-gray-900 dark:hover:text-red-500 hover:text-orange-500 dark:text-white"}>
+                <a href="https://drive.google.com/file/d/1NErmfbIJDiF-nls8NCa5bru9LB8VOKhV/view?usp=sharing" target={"_blank"} className={"ml-6 font-medium tracking-wider transition-colors text-gray-900 dark:hover:text-red-500 hover:text-orange-500 dark:text-white"}>
                     resume
                 </a>
                 ?
